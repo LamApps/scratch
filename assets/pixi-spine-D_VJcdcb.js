fecee226d98a30d17fde2e00939caf1fc9e556d8
lamapps <lamskills113@gmail.com>
2024-06-27 09:06:36 +0200

    buy ticket layout problem fixed

--- assets/pixi-spine-D_VJcdcb.js
+++
@@ -1,1 +0,0 @@
-import"./@pixi-spine-1RzxFvll.js";import"./@pixi-DoOeBslk.js";import"./eventemitter3-Bav5T-Ru.js";import"./earcut-DOWYkTK5.js";import"./url-J8SwAPIS.js";
---
+++ assets/pixi-spine-Dlv-9A3q.js
@@ -0,0 +1,1 @@
+import"./@pixi-spine-cYjAIX83.js";import"./@pixi-Dg4pj8dt.js";import"./eventemitter3-Bav5T-Ru.js";import"./earcut-DOWYkTK5.js";import"./url-J8SwAPIS.js";
